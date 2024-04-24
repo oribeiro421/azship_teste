a77a070f882f56dde92404e2f8630b6c68f6b3d4
otavioribeiro <otavioribeiro421@gmail.com>
2024-04-23 23:05:17 -0300

    terceiro commit

--- src/main/java/tavin/azship/gestaofretes/repository/FreightRepository.java
+++ src/main/java/tavin/azship/gestaofretes/repository/FreightRepository.java
@@ -1,7 +1,6 @@
 package tavin.azship.gestaofretes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tavin.azship.gestaofretes.model.Freight;
 
